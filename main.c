0f41c4d768c17518e1c33bcc11cbbb120ef48dfa
Denzel <denzel@capy.com>
2026-03-25 04:42:30 +0100

    ref: move typedefs

--- main.c
+++ main.c
@@ -1,23 +1,8 @@
-#include <stdbool.h>
-#include <stdint.h>
-
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef i8 b8;
-typedef i32 b32;
-
-typedef float f32;
-
 #define UNICODE
 #include <windows.h>
 #include <gl/gl.h>
+
+#include "base.h"
 #include "opengl.h"
 
 #define WGL_CONTEXT_MAJOR_VERSION_ARB 0x2091
